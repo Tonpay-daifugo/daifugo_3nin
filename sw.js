c087e214eb29e20053943c2bc564ca075351071c
Tonpay-daifugo <yk924-zweisec@yahoo.co.jp>
2026-09-06 20:18:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 const CACHE_PREFIX = "daifugo-3nin-cache-";
 const CACHE_NAME = CACHE_PREFIX + "v1";
+// バージョンを更新するたびに名前を変えること！
 
 const FILES_TO_CACHE = [
   "./",
@@ -21,25 +22,38 @@ self.addEventListener("install", (event) => {
 // 新しいService Workerが有効になったとき、古いキャッシュを削除
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((names) => {
-      return Promise.all(
-        names
-          .filter(
-            (name) =>
-              name.startsWith(CACHE_PREFIX) &&
-              name !== CACHE_NAME
-          )
-          .map((name) => caches.delete(name))
-      );
-    })
+    Promise.all([
+      caches.keys().then((names) => {
+        return Promise.all(
+          names
+            .filter(
+              (name) =>
+                name.startsWith(CACHE_PREFIX) &&
+                name !== CACHE_NAME
+            )
+            .map((name) => caches.delete(name))
+        );
+      }),
+      self.clients.claim()
+    ])
   );
 });
 
 // キャッシュがあればキャッシュを使用
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      if (event.request.mode === "navigate") {
+        return caches.match("./index.html");
+      }
+
+      return fetch(event.request);
     })
   );
 });
