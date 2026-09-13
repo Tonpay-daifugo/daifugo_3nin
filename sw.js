a9f8765e3bba31b17fa32e0a32f23e22e34b6487
Tonpay-daifugo <yk924-zweisec@yahoo.co.jp>
2026-09-13 23:08:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "daifugo-3nin-cache-";
-const CACHE_NAME = CACHE_PREFIX + "v376";
+const CACHE_NAME = CACHE_PREFIX + "v379";
 // バージョンを更新するたびに名前を変えること！
 
 const FILES_TO_CACHE = [
